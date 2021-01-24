836b94046947bc4d3fb1b73b95e7b754883878a5
moo-on <wns0159@gmail.com>
2021-01-24 14:10:49 +0900

    조합과순열 구현 연습.

--- strategy/perm,comb.py
+++ strategy/perm,comb.py
@@ -1,52 +1,84 @@
+#체크리스트와 DFS를 이용한 순열 출력, 문제점 : result가 저장이 안됌.
+
+def perm(n, r):
+    results =[]
+    checklist = [0]*len(n)
+    result= [0]*r
+    def DFS(L) :
+        #종료조건
+        if L== r:
+            print(result)
+            results.append(result[:])
+        #재귀문
+        else :
+            for i in range(len(n)):
+                if checklist[i]==0:
+                    result[L] =n[i]
+                    checklist[i] = 1
+                    DFS(L+1)
+        #백트래버스
+                    checklist[i] = 0
+    DFS(0)
+    return results
+
+print(perm([1,2,3],2))
+
+
+
+
 #조합1
-def comb(lst, n):
-    ret = []
-    if n > len(lst): return ret
-
-    if n == 1:
-        for i in lst:
-            ret.append([i])
-    elif n > 1:
-        for i in range(len(lst) - n + 1):
-            for temp in comb(lst[i + 1:], n - 1):
-                ret.append([lst[i]] + temp)
-
-    return ret
+# def comb(lst, n):
+#     ret = []
+#     if n > len(lst): return ret
+#
+#     if n == 1:
+#         for i in lst:
+#             ret.append([i])
+#     elif n > 1:
+#         for i in range(len(lst) - n + 1):
+#             for temp in comb(lst[i + 1:], n - 1):
+#                 ret.append([lst[i]] + temp)
+#
+#     return ret
 
 #순열1
-def perm(lst, n):
-    ret = []
-    if n > len(lst): return ret
+# def perm(lst, n):
+#     ret = []
+#     if n > len(lst): return ret
+#
+#     if n == 1:
+#         for i in lst:
+#             ret.append([i])
+#     elif n > 1:
+#         for i in range(len(lst)):
+#             temp = [i for i in lst]
+#             temp.remove(lst[i])
+#             for p in perm(temp, n - 1):
+#                 ret.append([lst[i]] + p)
+#    return ret
 
-    if n == 1:
-        for i in lst:
-            ret.append([i])
-    elif n > 1:
-        for i in range(len(lst)):
-            temp = [i for i in lst]
-            temp.remove(lst[i])
-            for p in perm(temp, n - 1):
-                ret.append([lst[i]] + p)
 
-    return ret
 
 #순열2
-def permute(lst, r):
-    results = []
-    prev_elements = []
+# def permute(lst, r):
+#     results = []
+#     prev_elements = []
+#
+#     def dfs(elements):
+#         if len(elements) ==(len(lst)-r):
+#             results.append(prev_elements[:])
+#
+#         for e in elements:
+#             next_elements = elements[:]
+#             next_elements.remove(e)
+#
+#             prev_elements.append(e)
+#             dfs(next_elements)
+#             prev_elements.pop()
+#
+#     dfs(lst)
+#     return
 
-    def dfs(elements):
-        if len(elements) ==(len(lst)-r):
-            results.append(prev_elements[:])
 
-        for e in elements:
-            next_elements = elements[:]
-            next_elements.remove(e)
 
-            prev_elements.append(e)
-            dfs(next_elements)
-            prev_elements.pop()
-
-    dfs(lst)
-    return results
 
--- test.py
+++ test.py
@@ -1,28 +1,11 @@
-def permute(lst, r):
-    results = []
-    prev_elements = []
 
-    def dfs(elements):
-        if len(elements) ==(len(lst)-r):
-            results.append(prev_elements[:])
 
-        for e in elements:
-            next_elements = elements[:]
-            next_elements.remove(e)
+tup = 'A','B', 'C'
+lst = list(tup)
+print(lst)
 
-            prev_elements.append(e)
-            dfs(next_elements)
-            prev_elements.pop()
-
-    dfs(lst)
-    return results
-
-#print(permute([], 2))
-
-#리스트 비었을시 FOR문 작동 X
-
-for i in []:
-    print(3)
+for i in lst:
+    print(i.swapcase())
 
 
 
@@ -51,25 +34,6 @@ for i in []:
 
 
 
-
-
-
-
-
-#체크리스트와 DFS를 이용한 순열 출력, 문제점 : result가 저장이 안됌.
-# def DFS(L) :
-#     #종료조건
-#     if L== r:
-#         print(result)
-#     #재귀문
-#     else :
-#         for i in range(len(n)):
-#             if checklist[i]==0:
-#                 result[L] =n[i]
-#                 checklist[i] = 1
-#                 DFS(L+1)
-#     #백트래버스
-#                 checklist[i] = 0
 
 
 
