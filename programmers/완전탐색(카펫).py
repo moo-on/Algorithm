c4921aff41511cbca5c6f70b84ca3fae144afc48
moo-on <wns0159@gmail.com>
2021-04-10 19:24:32 +0900

    완전탐색

---
+++ programmers/완전탐색(카펫).py
@@ -0,0 +1,19 @@
+def divisor(n):
+    lst=[]
+    for i in range(1, int(n**0.5)+1):
+        if n%i == 0:
+            lst.append([i, n//i])
+    return lst
+
+def solution(brown, yellow):
+
+    for lst in divisor(yellow):
+        if brown == (lst[0]*2 + lst[1]*2 + 4):
+            lst[0] += 2
+            lst[1] += 2
+            return list(reversed(lst))
+
+
+
+print(solution(8,1))
+
