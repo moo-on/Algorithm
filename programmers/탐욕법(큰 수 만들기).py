d36ff4f135461f179ebf8fcaa288fee706072a3b
moo-on <wns0159@gmail.com>
2021-05-03 23:07:44 +0900

    탐욕법.

--- programmers/탐욕법(큰 수 만들기).py
+++ programmers/탐욕법(큰 수 만들기).py
@@ -10,38 +10,48 @@ def solution(number, k):
         answer.append(int(string))
     return str(max(answer))
 '''
+
+
 def solution(number, k):
-    lst = list(map(int, number))
+    lst = number
     answers = []
     lst_f = lst[:k + 1]
     index_l = 0
 
     while k != 0:
-        max_n = max(lst_f)
-        index = lst_f.index(max_n)
-        index_l += index+1
-        k -= (index)
-
-        lst_f = lst[index_l: index_l + k+1]
-        answers.append(max_n)
-        print("lst_f: ", lst_f)
-        print("answers: ", answers)
-        print("index_l: ", index_l)
-        print("_______")
-        if len(lst_f) == 2: break
-    if len(answers) == len(lst) -k: return ''.join(map(str, answers))
+        index = lst_f.index(max(lst_f))
+        answers.append(lst_f[index])
+        index_l += index + 1
+        k -= index
+        lst_f = lst[index_l: index_l + k + 1]
+        if len(answers) == len(number) - k: return "".join(map(str, answers))
     answers.extend(lst[index_l:])
     return ''.join(map(str, answers))
 
 
-
-#number = "4177252841"  #원소 10개
 number = "99991"
-k = 3 # 7752841 3개 뽑아서 없애기 7개 원소 고르기
-#k = 4 # 775841 4개 뽑아서 없애기
-
-
-
-
-#print(solution(number, k ))
+number = '4177252841' #"775841"
+k = 4 # 7752841 4개 뽑아서 없애기 7개 원소 고르기
 print(solution(number,k))
+
+
+def solution(number, k):
+    # stack에 입력값을 순서대로 삽입
+    stack = [number[0]]
+    for num in number[1:]:
+        # 들어오는 값이 stack 값보다 크면, 기존의 값을 제거하고 새로운 값으로 바꿈
+        # 참고 : len(stack) > 0 == stack
+        while len(stack) > 0 and stack[-1] < num and k > 0:
+            # 값을 한개 빼서 k는 1이 제거
+            k -= 1
+            # 내부의 값을 제거
+            stack.pop()
+        # 새로운 값을 삽입
+        stack.append(num)
+    # 만일 충분히 제거하지 못했으면 남은 부분은 단순하게 삭제
+    # 이렇게 해도 되는 이유는 이미 큰 수부터 앞에서 채워넣었기 때문
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
+
+
