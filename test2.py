2f1ac7851c526dcc0c277aa7fee33b78b1ba04b1
moo-on <wns0159@gmail.com>
2021-06-17 22:32:37 +0900

    DFS.

--- test2.py
+++ test2.py
@@ -1,45 +1,41 @@
-# 3	[[1, 1, 0], [1, 1, 0], [0, 0, 1]]	2
+answer = 0
 
-n = 3
-computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+def solution(begin, target, words):
+    global answer
 
-def visit(k, graph, visited):
-    visited[k] = 1
-    for i in range(len(graph[k])):
-        if visited[i] == 0 and graph[k][i] == 1:
-            visit(i, graph, visited)
+    if target not in words:
+        return 0
+    visited = [0 for i in words]
+    dfs(begin, target, words, visited)
 
-def solution(n, computers):
-    answer = 0
-    visited = [0]*n
 
-    for i in range(n):
-        if visited[i] == 0:
-            visit(i, computers, visited)
-            answer += 1
-        if 0 not in visited:
-            break
+
+
     return answer
 
-# def visit(k, graph, visited):
-#     visited[k] = 1
-#     for i in range(len(graph[k])):
-#         if visited[i] == 0 and graph[k][i] == 1:
-#             visit(i, graph, visited)
+def dfs(begin, target, words, visited):
+    global answer
 
-# def solution(n, computers):
-#
-#     visited = [0] * n
-#
-#     answer = 0
-#
-#     for i in range(n):
-#         if visited[i] == 0:
-#             visit(i, computers, visited)
-#             answer += 1
-#         if 0 not in visited:
-#             break
-#
-#     return answer
+    stacks = [begin]
+    modify_lst = []
 
-print(solution(n, computers))
+    while stacks:
+
+        stack = stacks.pop(0)
+        n=0
+        if stack == target:
+            return answer
+        for i in range(len(words)):
+
+            if visited[i] == 1: continue
+            if len([s1 for s1, s2 in zip(words[i], stack) if s1 == s2]) == 1:
+                visited[i] = 1
+                stacks.append(words[i])
+                n += 1
+
+        modify_lst.append(n)
+
+        answer += 1
+
+a = solution("hit","cog",["hot", "dot", "dog", "lot", "log", "cog"])
+print(a)
