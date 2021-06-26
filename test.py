f2c72161e6edc44a2d705c4bd4f715ad698c85aa
moo-on <wns0159@gmail.com>
2021-06-26 23:49:56 +0900

    Stack&Queue

--- test.py
+++ test.py
@@ -3,20 +3,32 @@
 
 
 def solution(priorities, location):
-    answer = 0
-    location_id = id(priorities[location])
+    answer = []
+    MAX = max(priorities)
+    lst = [i for i in range(len(priorities))]
+    lst_answer = []
 
+    while priorities:
+        if MAX == priorities[0]:
+            answer.append(priorities[0])
+            del priorities[0]
 
+            lst_answer.append(lst[0])
+            del lst[0]
+
+            if not priorities: break
+            MAX = max(priorities)
+        else:
+            priorities.append(priorities[0])
+            del priorities[0]
+
+            lst.append(lst[0])
+            del lst[0]
+
+    print(answer)
+    print(lst_answer.index(location) + 1)
     return answer
 
-lst = [1,2,3]
-print(id(lst[1]))
-lst2 = []
-lst2.append(lst[1])
-print(id(lst2[0]))
-
-lst.append(lst[1])
-
-print(id(lst[-1]))
-lst.append(2)
-print(id(lst[-1]))
+if __name__ =="__main__":
+    solution([1, 1, 9, 1, 1, 1], 0)
+    solution([2, 1, 3, 2],2)
