e10dd8f0a16cb5900256188030feea26a778d6c6
moo-on <wns0159@gmail.com>
2021-06-28 23:51:35 +0900

    .

--- test.py
+++ test.py
@@ -1,34 +1,12 @@
-#[1, 1, 9, 1, 1, 1]	0	5
+lst = ['a','b','c']
 
+lst.sort(reverse = True)
 
+s = '44'
+s.count('a')
 
-def solution(priorities, location):
-    answer = []
-    MAX = max(priorities)
-    lst = [i for i in range(len(priorities))]
-    lst_answer = []
+s.isalnum()
 
-    while priorities:
-        if MAX == priorities[0]:
-            answer.append(priorities[0])
-            del priorities[0]
-
-            lst_answer.append(lst[0])
-            del lst[0]
-
-            if not priorities: break
-            MAX = max(priorities)
-        else:
-            priorities.append(priorities[0])
-            del priorities[0]
-
-            lst.append(lst[0])
-            del lst[0]
-
-    print(answer)
-    print(lst_answer.index(location) + 1)
-    return answer
-
-if __name__ =="__main__":
-    solution([1, 1, 9, 1, 1, 1], 0)
-    solution([2, 1, 3, 2],2)
+txt = "1D_1AS12"
+x = txt.isidentifier()
+print(x)
