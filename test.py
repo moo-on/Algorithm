7c536c189097634a1e65b60c4e4d30417b1fe72a
moo-on <wns0159@gmail.com>
2021-06-29 23:54:04 +0900

    .

--- test.py
+++ test.py
@@ -1,12 +1,26 @@
-lst = ['a','b','c']
+import sys
+input = sys.stdin.readline
 
-lst.sort(reverse = True)
+n = int(input())
+lst = [input().split() for _ in range(n)]
 
-s = '44'
-s.count('a')
+s = set()
 
-s.isalnum()
+for l in lst:
+    if l[0] =='add':
+        s.add(l[1])
+    elif l[0] == 'remove':
+        if l[1] in s: s.remove(l[1])
+    elif l[0] == 'check':
+        if l[1] in s: print(1)
+        else: print(0)
+    elif l[0] == 'toggle':
+        if l[1] in s: s.remove(l[1])
+        elif l[1] not in s: s.add(l[1])
+    elif l[0] == 'all':
+        s = set([i for i in range(1,21)])
+    elif l[0] == 'empty':
+        s= set()
 
-txt = "1D_1AS12"
-x = txt.isidentifier()
-print(x)
+
+
---
+++ test2.py
@@ -0,0 +1,8 @@
+lst =  {1,2,3}
+
+def s(l):
+    l.remove(5,)
+
+s(lst)
+
+print(lst)
