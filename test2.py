14f127471ffd60c3d465129d19bdfdaacf7a45cf
moo-on <wns0159@gmail.com>
2021-09-09 23:53:35 +0900

    add problem

--- test2.py
+++ test2.py
@@ -1,38 +1,10 @@
-# ["abab","bbaa","bababa","bbbabababbbaa"] [true,false,false,true]
 
-def bitwise(lst):
-    temp = lst[0]
-    for e in lst:
-        if temp & e == 0: return True
-        temp = temp & e
-    else: return False
+def solution(numbers):
+    answer = 0
+    _set = set(numbers)
 
-
-def BFS(lst, i):
-    for i in range(len(lst)):
-        lst[i] = lst[i] - 1
-        if bitwise(lst) == True:
-            return i
-
-
-
-def solution(m, b):
-    answer = []
-    index = 0
-    for i in m:
-        lst = b[:m]
-        b = b[m:]
-
-
-
+    for i in range(1,10):
+        if i not in _set: answer += i
     return answer
 
-print(solution([2, 2], [3, 2, 1, 2]))
-
-
-
-b =[1,2,3,4]
-m= b[:2]
-b= b[2:]
-
-print()
+print(solution([1,2,3,4,6,7,8,0]))
