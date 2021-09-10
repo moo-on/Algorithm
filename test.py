13a65d8690a7187ff98fb78f501597af4e2ed246
moo-on <wns0159@gmail.com>
2021-09-10 23:43:25 +0900

    add problem

--- test.py
+++ test.py
@@ -1,30 +1,52 @@
-# [[1, 6, 2, 5, 3, 4], [9, 9, 1, 0, 7, 8]]	22
+import heapq
 
-def solution(dice):
+def solution(jobs):
+    jobs.sort()
     answer = 0
-    set_lst = [set(l) for l in dice]
+    _heap = []
 
-    for n in range(1, 10000):
-        # 숫자 하나에 주사위 몇개 가능 한지 넣은 리스트
-        check_lst = [[] for j in range(len(dice))]
+    # 1. 기준점보다 작업요청이 빠른 작업들을 힙안에 넣어준다.
+    temp = 0
+    for job in jobs:
+        if temp >= job[0]:
 
-        # 숫자를 하나씩 체크해보기 위한 리스트
-        str_lst = list(map(int, list(str(n))))
+            # 1-1. 작업시간이 최소인 아이템을 선별하기 위해서 바꿔서 푸쉬해준다.
+            heapq.heappush(_heap, [job[1], job[0]])
+    # 2. 기준점보다 작업요청이 느린 작업을 만나면, 지금까지 힙에 쌓아논 아이템 중에 가장 최소 값을 빼내어서 현재 기준점을 업데이트 해준다. 그 후 heap 안에 push
+        elif temp < job[0]:
+            if len(_heap) == 0:
+                temp = job[0]
+                heapq.heappush(_heap, [job[1], job[0]])
 
-        # 숫자를 하나 씩 주사위의 숫자랑 비교
-        for i_1, e in enumerate(str_lst):
-            for i_2, l in enumerate(set_lst):
-                if e in l: check_lst[i_1].append(i_2)
+            items = heapq.heappop(_heap)
+            # 대기시간 더해주기.(순서를 바꿔서 넣은것 참고)
+            answer += (temp - items[1] + items[0])
+            heapq.heappush(_heap, [job[1], job[0]])
+            # 기준점 올려주기
+            temp += items[0]
 
-        # 숫자에 몇개의 주사위가 맵핑 되어있는지 확인
-        temp = set()
-        for l in check_lst:
-            for e in l:
-                temp.add(e)
-        # 맵핑된 숫자보다 작다면 해당 숫자 반환
-        if len(temp)<len(str_lst):
-            return n
-    return 0
+            # 2-1. 올라간 기준점으로 할 수 있는 2번 작업 다 해주기
+            if len(_heap) >=1:
+                while _heap[0][1] >= temp:
+                    items = heapq.heappop(_heap)
 
-print(solution([[0, 1, 5, 3, 9, 2], [2, 1, 0, 4, 8, 7], [6, 3, 4, 7, 6, 5]]))
-print(solution([[1, 6, 2, 5, 3, 4], [9, 9, 1, 0, 7, 8]]))
+                    # 대기시간 더해주기.(순서를 바꿔서 넣은것 참고)
+                    answer += (temp - items[1] + items[0])
+                    heapq.heappush(_heap, [job[1], job[0]])
+                    # 기준점 올려주기
+                    temp += items[0]
+                    # heap이 남아있지 않은 경우 에러 방지
+                    if len(_heap) ==0: break
+                    # 다시 heapq 넣기 반복
+
+    # for state 반복 후 heapq 안에 작업들 남아 있을 경우 처리.
+    while _heap:
+        items = heapq.heappop(_heap)
+        answer += (temp - items[1] + items[0])
+        temp += items[0]
+
+    return answer//len(jobs)
+
+
+
+print(solution([[1, 9], [1, 4], [1, 5], [1, 7], [1, 3]]))
--- test2.py
+++ test2.py
@@ -1,10 +1,3 @@
 
-def solution(numbers):
-    answer = 0
-    _set = set(numbers)
-
-    for i in range(1,10):
-        if i not in _set: answer += i
-    return answer
-
-print(solution([1,2,3,4,6,7,8,0]))
+lst = [[24, 10], [28, 39], [43, 20], [37, 5], [47, 22], [20, 47], [15, 2], [15, 34], [35, 43], [26, 1]]
+lst.sort()
