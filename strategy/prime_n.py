617e65ba7cd977e938b865930cb6a1b57a8f6bb2
moo-on <wns0159@gmail.com>
2021-09-11 23:49:58 +0900

    add problem

--- strategy/prime_n.py
+++ strategy/prime_n.py
@@ -1,10 +1,10 @@
 #에라토스테네스의 체
 
-n = 100
+n = 1200000000000
 a = [False, False] + [True] * (n - 1)
-primes = []
+primes = set()
 for i in range(2, n + 1):
     if a[i]:
-        primes.append(i)
+        primes.add(i)
         for j in range(2 * i, n + 1, i):
             a[j] = False
--- test.py
+++ test.py
@@ -9,7 +9,6 @@ def solution(jobs):
     temp = 0
     for job in jobs:
         if temp >= job[0]:
-
             # 1-1. 작업시간이 최소인 아이템을 선별하기 위해서 바꿔서 푸쉬해준다.
             heapq.heappush(_heap, [job[1], job[0]])
     # 2. 기준점보다 작업요청이 느린 작업을 만나면, 지금까지 힙에 쌓아논 아이템 중에 가장 최소 값을 빼내어서 현재 기준점을 업데이트 해준다. 그 후 heap 안에 push
@@ -26,8 +25,8 @@ def solution(jobs):
             temp += items[0]
 
             # 2-1. 올라간 기준점으로 할 수 있는 2번 작업 다 해주기
-            if len(_heap) >=1:
-                while _heap[0][1] >= temp:
+            if len(_heap) >= 1:
+                while temp <= _heap[0][1]:
                     items = heapq.heappop(_heap)
 
                     # 대기시간 더해주기.(순서를 바꿔서 넣은것 참고)
@@ -42,7 +41,9 @@ def solution(jobs):
     # for state 반복 후 heapq 안에 작업들 남아 있을 경우 처리.
     while _heap:
         items = heapq.heappop(_heap)
+        print("temp: ",temp)
         answer += (temp - items[1] + items[0])
+        print(items[0],"/",(temp - items[1] + items[0]))
         temp += items[0]
 
     return answer//len(jobs)
@@ -50,3 +51,4 @@ def solution(jobs):
 
 
 print(solution([[1, 9], [1, 4], [1, 5], [1, 7], [1, 3]]))
+# 2
