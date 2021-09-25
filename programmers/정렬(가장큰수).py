02d4fcc1a24b6713655ba64faa74206d82fa4e45
moo-on <wns0159@gmail.com>
2021-09-25 15:55:38 +0900

    add problem

---
+++ programmers/정렬(가장큰수).py
@@ -0,0 +1,62 @@
+# 원소의 값 : 0~1000
+case1 = [6, 10, 2]  # "6210"
+case2 = [3, 30, 34, 5, 9]  # "9534330"
+case3 = [1000, 999, 0, 0, 123]
+case4 = [87, 878]
+case5 = [89, 898]
+case5 = [0, 0]
+case6 = [9, 997, 99, 878, 87]
+case7 = [10, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+case8 = [412, 41]
+case9 = [303, 30]
+
+
+# 앞자리가 같은 문자들 전부 모아서 자릿수 맞춘 후 비교
+def solution(numbers):
+    answer = []
+    lst = [[] for _ in range(10)]
+    cnt_1000 = 0
+    cnt_0 = 0
+    # lst 안에 앞자리가 같은 문자들 모아주기
+    for number in numbers:
+        # 예외처리
+        if number == 1000:
+            cnt_1000 += 1
+            continue
+        if number == 0:
+            cnt_0 += 1
+            continue
+        start = int(str(number)[0])  # 원소의 앞자리 번호
+        end = int(str(number)[-1])  # 원소의 끝자리
+        if number >= 100:  # 3자리
+            lst[start].append([str(number), number])  # [기존 넘버, 비교용 자릿수 맞춰 준 넘버]
+        elif number >= 10:  # 2자리
+            if end >= start:
+                comparison_number = int(str(number).ljust(3, str(start))) + 0.1
+            else:
+                comparison_number = int(str(number).ljust(3, str(start))) - 0.1
+            lst[start].append([str(number), comparison_number])  # [기존 넘버, 비교용 자릿수 맞춰 준 넘버]
+        else:  # 1자리
+            lst[start].append([str(number), int(str(number).ljust(3, str(start)))])
+
+    lst.reverse()  # lst 배열을 뒤집어서 앞자리가 9부터 0까지 순서로 순회하기
+    for l in lst:
+        l.sort(key=lambda x: x[1], reverse=True)  # 9부터 순서 맞춰주기
+    # 순서가 맞춰졌으니, 그대로 붙여 준다.
+    for l in lst:
+        for e in l:
+            answer.append(e[0])
+    # 1000과 0 예외처리한것들 처리해주기
+    for i in range(cnt_1000):
+        answer.append("1000")
+    for i in range(cnt_0):
+        answer.append("0")
+    if answer[0] == "0": return "0"
+    return "".join(answer)
+
+
+print(solution(case8))
+print(solution(case4))
+
+# 87 878
+# 878 87
--- test.py
+++ test.py
@@ -1,33 +1,20 @@
-import heapq
+case1 = [3, 0, 6, 1, 5]  # 3
+case2 = [12,11,10,9,8,1] # 5
 
-case1 = ["I 16", "D 1"]
-case2 = ["I 7","I 5","I -5","D -1"]
-case3 = ["I 4", "I 3", "I 2", "I 1", "D 1", "D 1", "D -1", "D -1", "I 5", "I 6"] # return 6,5
-# 4 3 2 1 4x 3x 1x 2x
-def solution(operations):
-    cnt = 0
-    min_heap = []
-    max_heap = []
+# 인용횟수는 10000이하
+# 길이는 1000이하
+def solution(citations):
+    length = len(citations)
+    citations.sort()
+    if citations[-1] == 0 : return 0
+    for citation in citations:
+        if citation == length: return length
+        if citation > length:
+            return length
+        length -= 1
 
-    # 값 제거 연산 시, 큐에 있는것보다 삭제 연산이 같아지면 큐 초기화
-    for operation in operations:
-        if operation[0] == "I":
-            heapq.heappush(min_heap,int(operation[2:]))
-            heapq.heappush(max_heap, [-int(operation[2:]), int(operation[2:])])
-            cnt += 1
-        else: # D 연산 일 경우
-            if not min_heap or not max_heap: continue # 둘 중에 하나라도 비어 있다면, 연산 패스
-            if operation[2] == "1": heapq.heappop(max_heap)  # 최대 값 제거 연산
-            else: heapq.heappop(min_heap)  # 최소 값 제거 연산
-            # 연산 끝난 후 둘 중에 하나라도 큐가 비어 있다면 큐 둘다 초기화
-            cnt -= 1
-            if cnt == 0:
-                min_heap = []
-                max_heap = []
+# 0 1 3 5 6
+# 5 4 3 2 1
 
-    # 연산 다 종료 후
-    if not min_heap or not max_heap: return [0,0]
-    else: return [max_heap[0][1], min_heap[0]]
-
-
-print(solution(case3))
+print(solution(case2))
+print(solution(case1))
