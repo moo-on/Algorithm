06cca15b1c8179f082ed8b79b9a9e6e7581504e7
moo-on <wns0159@gmail.com>
2021-10-10 21:36:45 +0900

    add strategy

--- test.py
+++ test.py
@@ -1,35 +1,41 @@
 n1 = 6
 edge1 = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]	#3
 
-def DFS(answer_lst, check, edge, i, n):
+def DFS(answer_lst, edge, i, cnt,n):
     for e in edge[i]:
-        if check[e] == 1 :
-            continue
-        else:
-            check[e] = 1
-            if answer_lst[e] > n: answer_lst[e] = n
-            DFS(answer_lst, check, edge, e, n+1)
+        if cnt == len(answer_lst) - 1: break
+        elif e == 1:
+            if answer_lst[n] > cnt:
+                answer_lst[n] = cnt
+                break
+        DFS(answer_lst, edge, e, cnt +1, n)
 
 def solution(n, edge):
-    answer_lst  = [20002 for _ in range(n+1)]
-    check_lst = [0 for _ in range(n+1)]
+    answer_lst  = [30000 for _ in range(n+1)]
     lst = [set() for _ in range(n+1)]
 
     for e in edge:
         lst[e[0]].add(e[1])
         lst[e[1]].add(e[0])
 
-    print(lst)
+    for i in range(2, n+1):
+        DFS(answer_lst, lst, i,1,i)
 
-    DFS(answer_lst, check_lst, lst, 1,1)
-
-    print(answer_lst)
-    return 1
-
-
-
+    uniq = set(answer_lst)
+    MAX = sorted(uniq, reverse=True)[1]
+    answer = answer_lst.count(MAX)
+    return answer
 
 
+#solution(n1, edge1)
 
-
-solution(n1, edge1)
+print(solution(6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]), 3)
+print(solution(11, [[1, 2], [1, 3], [2, 4], [2, 5], [3, 5], [
+      3, 6], [4, 8], [4, 9], [5, 9], [5, 10], [6, 10], [6, 11]]), 4)
+print(solution(4, [[1, 2], [2, 3], [3, 4]]), 1)
+print(solution(2, [[1, 2]]), 1)
+print(solution(5, [[4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]), 2)
+print(solution(4, [[1, 2], [1, 3], [2, 3], [2, 4], [3, 4]]), 1)
+print(solution(4, [[1, 4], [1, 3], [2, 3], [2, 1]]), 3)
+print(solution(4, [[3, 4], [1, 3], [2, 3], [2, 1]]), 1)
+print(solution(4, [[4, 3], [1, 3], [2, 3]]), 2)
--- test2.py
+++ test2.py
@@ -1,18 +1,3 @@
-case1 = [6, 10, 2]  # "6210"
-case2 = [3, 30, 34, 5, 9]  # "9534330"
-case3 = [1000, 999, 0, 0, 123]
-case4 = [87, 878]
-case5 = [89, 898]
-case5 = [0, 0]
-case6 = [9, 997, 99, 878, 87]
-case7 = [10, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-case8 = [412, 41]
-case9 = [303, 30]
-
-def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3, reverse=True)
-    print(numbers)
-    return str(int(''.join(numbers)))
-
-solution(case1)
+def solution(n, edge):
+    answer = 0
+    return answer
