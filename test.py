b25c7ab5623c4c19d9cc78ac879c762a4fb142f0
moo-on <wns0159@gmail.com>
2021-11-17 23:55:53 +0900

    Add problem

--- test.py
+++ test.py
@@ -1,25 +1,18 @@
-# [44, 1, 0, 0, 31, 25]	[31, 10, 45, 1, 6, 19]	[3, 5]
-def solution(lottos, win_nums):
-    zero_cnt = 0
-    level = 1
-    for i in lottos:
-        if i == 0:
-            zero_cnt += 1
-        elif i not in win_nums:
-            level += 1
-
-    if level == 7: cnt = 6
-
-    high, low = level, level + zero_cnt
-
-    if low > 6:
-        low = 6
-    if high > 6:
-        high = 6
-
-    answer = [high, low]
-
-    return answer
-
-
-print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
+def solution(tickets):
+    tickets.sort(reverse=True)
+    routes = dict()
+    for t1, t2 in tickets:
+        if t1 in routes:
+            routes[t1].append(t2)
+        else:
+            routes[t1] = [t2]
+    st = ['ICN']
+    ans = []
+    while st:
+        top = st[-1]
+        if top not in routes or len(routes[top])==0:
+            ans.append(st.pop())
+        else:
+            st.append(routes[top].pop())
+    ans.reverse()
+    return ans
