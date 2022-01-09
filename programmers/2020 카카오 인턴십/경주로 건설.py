9ebb451675140a4eef64a79ab6ec1c091a463379
moo-on <wns0159@gmail.com>
2022-01-09 23:43:34 +0900

    Add problem

--- programmers/2020 카카오 인턴십/경주로 건설.py
+++ programmers/2020 카카오 인턴십/경주로 건설.py
@@ -1,53 +1,66 @@
 '''
-dfs 종료 조건
+종료 조건
     해당 경로가 더 비싼 루트
     벽에 막혀있다. & 격자 보다 크다
     목적지에 도착했을 때
 
 다음 함수 부를 때
-    방향성에 대한 비용 증가
-    내가 가진 방향
+    비용(cost)
+    위치(z, y, x)
+    방향(dir)
+z축 : right = 0, left = 1, up = 2, down = 3
 '''
+from collections import deque
 
 
 def solution(board):
-    answer = [999999999,0]
-
-    def dfs(y, x, cost, dir, lst):
-        if x < 0 or x >= len_ or y < 0 or y >= len_:
-            return
-        elif board_cost[y][x] <= cost:
-            return
-        elif board[y][x] == 1:
-            return
-        elif x == len_ - 1 and y == len_ - 1:
-            if answer[0] > cost:
-                answer[0] = cost
-                answer[1] = lst
-            return
-
-        board_cost[y][x] = cost
-        lst.append([y,x])
-        if dir == "row":
-            dfs(y, x + 1, cost + 100, "row", lst)
-            dfs(y, x - 1, cost + 100, "row", lst)
-            dfs(y + 1, x, cost + 600, "col", lst)
-        if dir == "col":
-            dfs(y, x + 1, cost + 600, "row", lst)
-            dfs(y, x - 1, cost + 600, "row", lst)
-            dfs(y + 1, x, cost + 100, "col", lst)
-
+    answer = 999999999
     len_ = len(board)
-    board_cost = [[99999 for _ in range(len_)] for _ in range(len_)]
+    board_cost = [[[99999 for _ in range(len_)] for _ in range(len_)] for _ in range(4)]
+    # z  y  x  cost  dir
+    q = deque([[0, 0, 1, 100], [3, 1, 0, 100]])
 
-    dfs(1, 0, 100, "col",[])
-    board_cost = [[99999 for _ in range(len_)] for _ in range(len_)]
-    dfs(0, 1, 100, "row",[])
+    while q:
+        temp = q.popleft()
+        z, y, x, cost = temp[0], temp[1], temp[2], temp[3]
 
-    return answer[0]
+        # termination condition
+        if x < 0 or x >= len_ or y < 0 or y >= len_:  # out of range
+            continue
+        elif board_cost[z][y][x] < cost:  # cost more expensive than dp
+            continue
+        elif board[y][x] == 1:  # encounter of wall
+            continue
+        elif x == len_ - 1 and y == len_ - 1:  # encounter of destination
+            if answer > cost:
+                answer = cost
+            continue
+
+        # pass the condition
+        board_cost[z][y][x] = cost
+
+        # if dir == "row"
+        if z == 0 or z == 1:
+            q.append([0, y, x + 1, cost + 100])
+            q.append([1, y, x - 1, cost + 100])
+            q.append([3, y + 1, x, cost + 600])
+            q.append([2, y - 1, x, cost + 600])
+        elif z == 2 or z == 3:
+            q.append([0, y, x + 1, cost + 600])
+            q.append([1, y, x - 1, cost + 600])
+            q.append([3, y + 1, x, cost + 100])
+            q.append([2, y - 1, x, cost + 100])
+
+    return answer
 
 
-#print(solution([[0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 0], [0, 0, 1, 0, 0, 0], [1, 0, 0, 1, 0, 1], [0, 1, 0, 0, 0, 1],
-               # [0, 0, 0, 0, 0, 0]]))
+# print(solution([[0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 0], [0, 0, 1, 0, 0, 0], [1, 0, 0, 1, 0, 1], [0, 1, 0, 0, 0, 1],
+# [0, 0, 0, 0, 0, 0]]))
 
-print(solution([[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]))
+# print(solution([[0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0],
+#                 [0, 0, 0, 1, 0, 0, 0, 1], [0, 0, 1, 0, 0, 0, 1, 0], [0, 1, 0, 0, 0, 1, 0, 0],
+#                 [1, 0, 0, 0, 0, 0, 0, 0]]))
+
+print(solution([[0, 0, 1, 0], [0, 0, 0, 0], [0, 1, 0, 1], [1, 0, 0, 0]]))
+
+# print(solution([[0,0,0,0,0,0,0,0],[1,0,1,1,1,1,1,0],[1,0,0,1,0,0,0,0],[1,1,0,0,0,1,1,1],[1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,0],[1,1,1,1,1,1,1,0],[1,1,1,1,1,1,1,0]]))
--- test3.py
+++ test3.py
@@ -1,53 +1,61 @@
 '''
-dfs 종료 조건
+종료 조건
     해당 경로가 더 비싼 루트
     벽에 막혀있다. & 격자 보다 크다
     목적지에 도착했을 때
 
 다음 함수 부를 때
-    방향성에 대한 비용 증가
-    내가 가진 방향
+    비용(cost)
+    위치(z, y, x)
+    방향(dir)
+z축 : row = 0, column = 1
 '''
+from collections import deque
 
 
 def solution(board):
-    answer = [999999999,0]
-
-    def dfs(y, x, cost, dir, lst):
-        if x < 0 or x >= len_ or y < 0 or y >= len_:
-            return
-        elif board_cost[y][x] <= cost:
-            return
-        elif board[y][x] == 1:
-            return
-        elif x == len_ - 1 and y == len_ - 1:
-            if answer[0] > cost:
-                answer[0] = cost
-                answer[1] = lst
-            return
-
-        board_cost[y][x] = cost
-        lst.append([y,x])
-        if dir == "row":
-            dfs(y, x + 1, cost + 100, "row", lst)
-            dfs(y, x - 1, cost + 100, "row", lst)
-            dfs(y + 1, x, cost + 600, "col", lst)
-        if dir == "col":
-            dfs(y, x + 1, cost + 600, "row", lst)
-            dfs(y, x - 1, cost + 600, "row", lst)
-            dfs(y + 1, x, cost + 100, "col", lst)
-
+    answer = -1
     len_ = len(board)
-    board_cost = [[99999 for _ in range(len_)] for _ in range(len_)]
+    board_cost = [[[-1 for _ in range(len_)] for _ in range(len_)] for _ in range(2)]
+    # z  y  x  cost  dir
+    q = deque([[0, 0, 1, 100], [1, 1, 0, 100]])
 
-    dfs(1, 0, 100, "col",[])
-    board_cost = [[99999 for _ in range(len_)] for _ in range(len_)]
-    dfs(0, 1, 100, "row",[])
+    while q:
+        temp = q.popleft()
+        z, y, x, cost = temp[0], temp[1], temp[2], temp[3]
 
-    return answer[0]
+        # termination condition
+        if x == len_ - 1 and y == len_ - 1:  # encounter of destination
+            if answer > cost or answer == -1:
+                answer = cost
+            continue
+        elif x < 0 or x >= len_ or y < 0 or y >= len_:  # out of range
+            continue
+        elif board_cost[z][y][x] == -1 or board_cost[z][y][x] <= cost:  # cost more expensive than dp
+            continue
+        elif board[y][x] == 1:  # encounter of wall
+            continue
 
 
-#print(solution([[0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 0], [0, 0, 1, 0, 0, 0], [1, 0, 0, 1, 0, 1], [0, 1, 0, 0, 0, 1],
-               # [0, 0, 0, 0, 0, 0]]))
+        # pass the condition
+        board_cost[z][y][x] = cost
 
-print(solution([[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]))
+        # if dir == "row"
+        if z == 0:
+            q.append([0, y, x + 1, cost + 100])
+            q.append([0, y, x - 1, cost + 100])
+            q.append([1, y + 1, x, cost + 600])
+            q.append([1, y - 1, x, cost + 600])
+        elif z == 1:
+            q.append([0, y, x + 1, cost + 600])
+            q.append([0, y, x - 1, cost + 600])
+            q.append([1, y + 1, x, cost + 100])
+            q.append([1, y - 1, x, cost + 100])
+
+    return answer
+
+
+
+
+print(solution([[0, 0, 1, 0], [0, 0, 0, 0], [0, 1, 0, 1], [1, 0, 0, 0]]))
+# print(solution([[0,0,0,0,0,0,0,0],[1,0,1,1,1,1,1,0],[1,0,0,1,0,0,0,0],[1,1,0,0,0,1,1,1],[1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,0],[1,1,1,1,1,1,1,0],[1,1,1,1,1,1,1,0]]))
