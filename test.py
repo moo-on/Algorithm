2531edf26b0306ed6da64f4fb9f9dd28921f404d
moo-on <wns0159@gmail.com>
2022-01-26 23:58:33 +0900

    Add problem

--- test.py
+++ test.py
@@ -1,59 +1,63 @@
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
 from collections import deque
 
 
 def solution(n, k, cmd):
     answer = ''
-    read_only = [i for i in range(n)]
-    undo = deque([])
-    log = []
+    link_dict = {}
+    for i in range(n):
+        link_dict[i] = {"prev": i - 1, "next": i + 1}
+    link_dict[0]["prev"], link_dict[n - 1]["next"] = "head", "tail"
+    link_dict["head"] = {next: 0}
 
+    print(link_dict)
+
+    undo = []
     for command in cmd:
         c = command.split()
 
         if c[0] == "U":
-            c[1] = int(c[1])
-            k -= c[1]
+            for _ in range(int(c[1])):
+                k = link_dict[k]["prev"]
 
         if c[0] == "D":
-            c[1] = int(c[1])
-            k += c[1]
+            for _ in range(int(c[1])):
+                k = link_dict[k]["next"]
 
         if c[0] == "C":
-            undo.append(read_only.pop(k))
-            log.append(k)
+            prev, next = link_dict[k]["prev"], link_dict[k]["next"]
+            undo.append(k)
 
-            if len(read_only) - 1 == k:  # last section
-                k -= 1
-                log[-1] = "end"
+            if prev == "head":
+                link_dict[prev]["next"] = link_dict[k]["next"]
+                link_dict[next]["prev"] = link_dict[k]["prev"]
+                k = link_dict[k]["next"]
+                continue
+
+            if next == "tail":
+                link_dict[prev]["next"] = link_dict[k]["next"]
+                k = link_dict[k]["prev"]
+                continue
+
+            link_dict[prev]["next"] = link_dict[k]["next"]
+            link_dict[next]["prev"] = link_dict[k]["prev"]
+            k = link_dict[k]["next"]
 
         if c[0] == "Z":
-            u = undo.pop()
-            l = log.pop()
+            node = undo.pop()
+            prev, next = link_dict[node]["prev"], link_dict[node]["next"]
 
-            if u < read_only[k]:
-                k += 1
-            if l == "end":
-                read_only.append(u)
+            if prev == "head":
+                link_dict[next]["prev"] = node
                 continue
-            read_only.insert(l, u)
 
-        answer = ["X" for _ in range(n)]
-        for e in read_only:
-            answer[e] = "O"
+            if next == "tail":
+                link_dict[prev]["next"] = node
+                continue
 
-    return ''.join(answer)
+            link_dict[prev]["next"] = node
+            link_dict[next]["prev"] = node
+
+    return link_dict
 
 
-print(solution(8, 2, ["D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"]))
+print(solution(8, 2, ["D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C"]))
